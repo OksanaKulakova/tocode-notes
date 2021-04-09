7beada34f6a007cf90eab51a9cbfa9064e34f86a
Evgenii Vedegis <tichoo@yandex.ru>
2021-04-09 11:27:09 +0800

    added tagsList

--- src/components/Notes/Form.vue
+++ src/components/Notes/Form.vue
@@ -1,18 +1,25 @@
 <template>
   <form class="note-form" @submit.prevent="onSubmit">
     <textarea required v-model="value" placeholder="Type ur note" />
+    <TagsList @onTagClick="handleTagClick" :items="tags" />
     <button class="btn btnPrimary" type="submit">Add new note</button>
   </form>
 </template>
 
 <script>
+import TagsList from '@/components/TagsList.vue'
 export default {
+  components: { TagsList },
   data() {
     return {
-      value: ''
+      value: '',
+      tags: ['home', 'work', 'travel']
     }
   },
   methods: {
+    handleTagClick(tag) {
+      console.log(tag)
+    },
     onSubmit() {
       this.$emit('onSubmit', this.value)
       this.value = ''
@@ -26,5 +33,8 @@ export default {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  textarea {
+    margin-bottom: 0;
+  }
 }
 </style>
